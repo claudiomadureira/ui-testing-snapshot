63c3a350ac8022e070b113e4185f6aef6dfda6ed
Cláudio Madureira <claudiomsilvaf@gmail.com>
2020-02-07 10:09:13 -0300

    first commit

---
+++ UITestingSnapshot/UITestingSnapshot/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  UITestingSnapshot
+//
+//  Created by Claudio Madureira Silva Filho on 2/7/20.
+//  Copyright © 2020 Claudio Madureira Silva Filho. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+     
+        return true
+    }
+
+
+}
+
---
+++ UITestingSnapshot/UITestingSnapshot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITestingSnapshot
+//
+//  Created by Claudio Madureira Silva Filho on 2/7/20.
+//  Copyright © 2020 Claudio Madureira Silva Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
